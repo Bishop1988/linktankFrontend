ac5802a5531b27d2ac10d40bd45476a393c9d8dc
Michael Bishop <michaelabishop1988@gmail.com>
2022-05-18 17:36:04 +0100

    changed folder stucture

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import "../styles/navbar.css"
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar_container">
+            <h1 className="navbar_title">Navbar</h1>
+        </div>
+     )
+}
+ 
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import "../styles/home.css"
+
+const Home = () => {
+    return ( 
+        <div className="home_container">
+            <h1>Home Page</h1>
+        </div>    
+    )
+}
+ 
+export default Home;
