1dc8dea577ed3ae71008d81977ed4f4618e26842
arician <arician@live.com>
2022-05-19 13:58:28 +0100

    basic footer sorted

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "../styles/footer.css"
+
+const Footer = () => {
+    return ( 
+        <div className="footer_container">
+            <h1 className="footer_title">&copy;Linktank 2022</h1>
+        </div>
+     )
+}
+ 
+export default Footer;
