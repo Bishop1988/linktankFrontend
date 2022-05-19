f06901f71b9e8ff95f159ee1949eab99ec0d975c
arician <arician@live.com>
2022-05-19 12:36:02 +0100

    buttons replaced with anchors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,15 @@ const Navbar = () => {
         <div className="navbar_container">
             <div className="navbar_leftside">
                 <h1 className="navbar_title">Linktank</h1>
-                {/* {loggedin && <button className="navbar_mypage">My page</button>} This is for conditional rendering later*/}
+                {/* {loggedin && <a className="navbar_mypage">My page</a>} This is for conditional rendering later*/}
             </div>
             <div className="navbar_rightside">
                 {/* {loggedin ? */}
-                <button className="navbar_button">Sign up</button>
-                <button className="navbar_button">Log in</button>
+                <a className="navbar_button">Sign up</a>
+                <a className="navbar_button">Log in</a>
                 {/* : */}
-                {/* <button className="navbar_button>Account settings</button>"*/}
-                {/* <button className="navbar_button">Log out</button>*/}
+                {/* <a className="navbar_button>Account settings</a>"*/}
+                {/* <a className="navbar_button">Log out</a>}*/}
                 {/* again these will both be for conditional rendering later, need to know how the signing in will work properly first*/}
             </div>
         </div>
