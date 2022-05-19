0e6a871b44a8bdf682def2ad295881de14f6a01a
arician <arician@live.com>
2022-05-19 12:27:50 +0100

    Header base, needs tweeks for conditional rendering later

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,19 @@ import "../styles/navbar.css"
 const Navbar = () => {
     return ( 
         <div className="navbar_container">
-            <h1 className="navbar_title">Navbar</h1>
+            <div className="navbar_leftside">
+                <h1 className="navbar_title">Linktank</h1>
+                {/* {loggedin && <button className="navbar_mypage">My page</button>} This is for conditional rendering later*/}
+            </div>
+            <div className="navbar_rightside">
+                {/* {loggedin ? */}
+                <button className="navbar_button">Sign up</button>
+                <button className="navbar_button">Log in</button>
+                {/* : */}
+                {/* <button className="navbar_button>Account settings</button>"*/}
+                {/* <button className="navbar_button">Log out</button>*/}
+                {/* again these will both be for conditional rendering later, need to know how the signing in will work properly first*/}
+            </div>
         </div>
      )
 }
