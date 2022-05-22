eb84967341c735511f5477cf64604d0c97038eaa
sammi-turner <ms.sammi.turner@gmail.com>
2022-05-22 13:33:34 +0100

    fix typos

--- src/components/HomeLinks.js
+++ src/components/HomeLinks.js
@@ -3,10 +3,10 @@ import "../styles/homelinks.css";
 export const HomeLinks = () => {
     return <div className="home_links">
         <div className="home_link">
-            <a href="../pages/Login" alt="Log in">LOG IN</a>
+            <a href="../pages/login" alt="Log in">LOG IN</a>
         </div>
         <div className="home_link">
-            <a href="../pages/Signup" alt="Sign up">SIGN UP</a>
+            <a href="../pages/signup" alt="Sign up">SIGN UP</a>
         </div>
     </div>
 };
