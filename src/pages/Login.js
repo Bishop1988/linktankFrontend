83be0e877e198392edae562fa35fbdceb49dfcb5
Nezumi07 <limonsee@gmail.com>
2022-05-26 10:01:45 +0100

    Login functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,57 @@
+import { useState } from "react";
+import  LoginForm  from "../components/LoginForm";
+
 import "../styles/login.css"
 
-const Login = () => {
-  return (
-    <h1>This is the login page</h1>
-  )
+
+function App () {
+  const adminUser = {
+    email: "a@a.com",
+    password: "a123"
+  }
+
+
+const [user, setUser] = useState ({name: "", email: ""});
+const [error,setError] = useState ("");
+
+const Login = details => {
+  console.log(details);
+
+  if (details.email == adminUser.email && details.password == adminUser.password){
+    console.log ("Logged In");
+  setUser({
+     name: details.name,
+     email: details.email 
+    });
+  } else {
+    console.log("Details do not match");
+    setError("WRONG DETAILS MATE!")
+  }
 }
 
-export default Login;
+
+
+
+  const Logout = () => {
+    setUser ({name: "", email: ""});
+  console.log("Logout");
+}
+
+
+return (
+  <div className="App">
+    {(user.email !="") ? (
+    <div className="welcome">
+      <h2>Welcome <span>{user.name}</span></h2>
+      <button onClick={Logout}>Logout</button>
+    </div>
+    
+) : (
+  <LoginForm Login={Login} error={error}/>
+)}
+</div>
+
+)}
+
+
+export default App
